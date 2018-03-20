ea0db85eef1d7aac271ebb4b62498acd0f90b4de
Degwy <degwy@github.com>
2018-03-20 16:24:52 +0200

    add helper

--- Gemfile
+++ Gemfile
@@ -5,5 +5,10 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "rails"
-gem 'rspec'
 gem 'rake'
+
+
+group :test do
+  gem "rspec"
+  gem 'colorize'
+end
---
+++ spec/helpers/colorizing_helper.rb
@@ -0,0 +1,33 @@
+require 'colorize'
+require 'time'
+
+String.disable_colorization = false
+
+module ColorizingThings
+
+  def p_msg(msg,clr=:light_blue,wcr=false,wtime=true)
+    strtime = ''
+    strtime = "[#{Time.now.strftime('%X')}] ".colorize(:light_cyan) if wtime
+    msgout = msg.colorize(clr)
+    print strtime + msgout
+    puts if wcr
+  end
+
+  def p_done
+    puts ' ...done'.colorize(:light_green)
+  end
+
+  def p_wrote(n)
+    puts " ...#{n} items ...done.".colorize(:light_green)
+  end
+
+  def p_str(str,color=:light_green,wcr=false)
+    print str.colorize(color)
+    puts if wcr
+  end
+
+  def fatal_error(msg)
+    p_msg "#{msg} EXITING", :red, true
+    exit
+  end
+end
